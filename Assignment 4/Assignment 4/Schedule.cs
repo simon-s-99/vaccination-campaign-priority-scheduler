ae4400bb78f8dde6558792f35eb840e9d77bd1f6
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2023-10-26 13:57:46 +0200

    fixed startdate date format in ScheduleMenu

--- Assignment 4/Assignment 4/Schedule.cs
+++ Assignment 4/Assignment 4/Schedule.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -78,7 +79,8 @@ namespace Schedule
 
                 int scheduleMenu = Vaccination.Program.ShowMenu("", new[]
                 {
-                    $"Startdatum: {newSchedule.StartDate}",
+                    $"Startdatum: " +
+                    $"{newSchedule.StartDate.ToString("g", CultureInfo.CreateSpecificCulture("sv-SE"))}",
                     $"Starttid: {newSchedule.StartTime}",
                     $"Sluttid: {newSchedule.EndTime}",
                     $"Antal samtidiga vaccinationer: {newSchedule.ConcurrentVaccinations}",
